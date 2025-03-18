35f97b86316375f0aacc1df85046457595fcf7f6
Arjun <59514930+arjundha@users.noreply.github.com>
2025-03-17 20:00:56 -0700

    Slimes can die in a puff of smoke

--- mob.gd
+++ mob.gd
@@ -6,10 +6,26 @@ extends CharacterBody2D
 #func _ready() -> void:
 	#player = get_node("/root/Game/Player")
 
+var health = 3
+const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+
 @onready var player = get_node("/root/Game/Player")
+
+func _ready() -> void:
+	%Slime.play_walk()
 
 func _physics_process(delta: float) -> void:
 	# global_position is where the current entitity is in the game world
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
+	
+func take_damage():
+	health -= 1
+	%Slime.play_hurt()
+	
+	if health <= 0:
+		queue_free()
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
